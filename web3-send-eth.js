2df11f7f60bd59671d66e60e787c6cb22e7ec43e
Islarky Overlord <islarky88@gmail.com>
2021-05-30 15:38:50 +0800

    sendTransaction working on ganache

--- web3-send-eth.js
+++ web3-send-eth.js
@@ -1,34 +1,52 @@
+require('dotenv').config({ path: '.env.dev' });
+
 const Web3 = require('web3');
 
-const testnet = `https://kovan.infura.io/v3/2a98a1e540c64ec8904e8c056e2e5fc2`;
-const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+// const ganache = require('ganache');
+// const ganache = new web3.providers.HttpProvider('http://127.0.0.1:8545');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+// const testnet = `https://kovan.infura.io/v3/2a98a1e540c64ec8904e8c056e2e5fc2`;
+// const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
 
 // console.log(web3.eth);
 
 const main = async () => {
-    const account = web3.eth.accounts.create();
-    console.log('account', account);
+    // const account = web3.eth.accounts.create();
+    // console.log('account', account);
 
     const currentAccount = web3.eth.accounts.privateKeyToAccount(
-        '0x71ed7f7d5488f6f27ce7da610ded762d6f1d39f5ce0b861bc826b040182e0be0',
+        process.env.PRIV_KEY,
     );
     console.log('currentAccount', currentAccount);
 
+    let balance = await web3.eth.getBalance(currentAccount.address); //Will give value in.
+    // balance = web3.utils.toWei(balance, 'ether');
+    balance = Number(web3.utils.fromWei(balance));
+    console.log(balance);
+
     try {
-        const ownerAddress = '0x008098A525E61F932314216634597815B976853B';
         const passw =
             'fd70870ab9ae9a6bcc0de8f8e980bf9e93299af937ee8e1706ded00f82c78d98';
 
-        web3.eth.personal.unlockAccount(ownerAddress, passw);
+        const unlockResult = await web3.eth.personal.unlockAccount(
+            currentAccount.address,
+            passw,
+        );
 
-        const toAddress = '0x2682c0bA7cdb28A59DD872f9f060e973fff9532E'; // Address of the recipient
-        const amount = '2'; // Willing to send 2 ethers
-        const amountToSend = web3.utils.toWei(amount, 'ether'); // Convert to wei value
-        var send = web3.eth.sendTransaction({
-            from: addr,
+        console.log('unlockResult', unlockResult);
+
+        const toAddress = '0x0DC42c645C9115CC6a7221458f073f45E52d2Ce8'; // Address of the recipient
+        const etherToSend = '0.1';
+        const sendResult = await web3.eth.sendTransaction({
             to: toAddress,
-            value: amountToSend,
+            gasPrice: '20000000000',
+            gas: '21000',
+            from: toAddress,
+            value: web3.utils.toWei(etherToSend, 'ether'),
         });
+
+        console.log(sendResult);
     } catch (error) {
         console.log(error.message);
     }
