b6f98529bbd9e24b3190f83c5621ac412a92f4f3
Islarky Overlord <islarky88@gmail.com>
2021-05-31 09:45:10 +0800

    send eth successful on ethers library

--- ethers-send-eth.js
+++ ethers-send-eth.js
@@ -1,22 +1,22 @@
-require('dotenv').config({ path: '.env.dev' });
+require('dotenv').config({ path: '.env' });
 
 const { ethers, utils, BigNumber } = require('ethers');
 
-let provider = ethers.getDefaultProvider('kovan');
-
 const myPrivateKey = process.env.METAMASK_PASSWORD;
 const myAddress = '0x008098A525E61F932314216634597815B976853B';
-const receiver = '0x53CccA398F6CD117e6aa34AB71598b8F172Bf0FF';
+const receiverAddress = '0x53CccA398F6CD117e6aa34AB71598b8F172Bf0FF';
+const amountToSend = '0.0042069';
+
+const provider = ethers.getDefaultProvider('kovan');
+
+const wallet = new ethers.Wallet(myPrivateKey, provider);
 
 const main = async () => {
-    // console.log('provider', provider);
-
-    const wallet = new ethers.Wallet(myPrivateKey, provider);
-
-    console.log(wallet);
-
     const gasPrice = await wallet.getBalance();
     console.log('gasPrice', gasPrice);
+
+    const getBlockNumber = await provider.getBlockNumber();
+    console.log('getBlockNumber', getBlockNumber);
 
     let balance = await provider.getBalance(myAddress);
     balance = ethers.utils.formatEther(balance, 'ether');
@@ -28,17 +28,24 @@ const main = async () => {
     // let bignum = new BigNumber(balance);
     // console.log('bignum', bignum);3
 
-    // Acccounts now exposed
-    const params = [
-        {
-            from: myAddress,
-            to: receiver,
-            value: ethers.utils.parseUnits('0.0006', 'ether').toHexString(),
-        },
-    ];
+    sendEther();
+};
 
-    const transactionHash = await provider.send('eth_sendTransaction', params);
-    console.log('transactionHash is ' + transactionHash);
+const sendEther = async () => {
+    console.log(
+        `Attempting to send transaction from ${wallet.address} to ${receiverAddress}`,
+    );
+
+    // Create Tx Object
+    const tx = {
+        to: receiverAddress,
+        value: ethers.utils.parseEther(amountToSend),
+    };
+
+    // Sign and Send Tx - Wait for Receipt
+    const createReceipt = await wallet.sendTransaction(tx);
+    await createReceipt.wait();
+    console.log(`Transaction successful with hash: ${createReceipt.hash}`);
 };
 
 main();
