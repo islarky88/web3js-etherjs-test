528440cb8b5ea1eb6046aae2581a5cf36caf4a89
Islarky Overlord <islarky88@gmail.com>
2021-07-08 13:21:18 +0800

    add express to repo

--- multicall.js
+++ multicall.js
@@ -1,5 +1,8 @@
 require('dotenv').config({ path: '.env' });
 const Web3 = require('web3');
+const express = require('express');
+const app = express();
+const port = 3000;
 const { UNI_V3_POS } = require('./abi/uniswap.js');
 
 const fromAddress = '0x008098A525E61F932314216634597815B976853B';
@@ -15,12 +18,18 @@ const myPrivateKey = process.env.METAMASK_PASSWORD;
 
 const univ3pos = '0xc36442b4a4522e871399cd717abdd847ab11fe88';
 
-const main = async () => {
+const contract = new web3.eth.Contract(UNI_V3_POS, univ3pos);
+
+const methods = [];
+
+app.get('/', (req, res) => {
+    res.send(`Multicall Uniswap V3
+      <br/><a href="/create-pool">create pool</a>
+      <br/><a href="/remove-pool">remove pool</a>
+  `);
+});
+app.get('/create-pool', async (req, res) => {
     try {
-        const contract = new web3.eth.Contract(UNI_V3_POS, univ3pos);
-
-        const methods = [];
-
         // add mint method first
         const params = [
             '0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb', // token0 - InstaToken Address
@@ -62,11 +71,62 @@ const main = async () => {
         const sendResult = await web3.eth.sendSignedTransaction(
             signResult.rawTransaction,
         );
-        console.log(sendResult);
+        res.send(sendResult);
     } catch (error) {
         console.log(error);
-        // console.log(error.message);
+        res.send(error);
     }
-};
+});
+app.get('/remove-pool', async (req, res) => {
+    try {
+        // add mint method first
+        const params = [
+            '0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb', // token0 - InstaToken Address
+            '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // token1 - WETH9 address
+            '123', // fee
+            '456', // tickLower
+            '789', // tickUpper
+            '249999999999999999716', // amount0Desired
+            '856568008741777183', // amount1Desired
+            '242154525503193061571', // amount0Min
+            '831197723551503890', // amount1Min
+            '0x7284a8451d9a0e7Dc62B3a71C0593eA2eC5c5638', // recipient - smart contract address of InstaToken
+            '1628718978', // deadline
+        ];
+        const mintEncoded = contract.methods.mint(params).encodeABI();
+        methods.push(mintEncoded);
 
-main();
+        // add then refundETH next
+        const refund = contract.methods.refundETH().encodeABI();
+        methods.push(refund);
+
+        const inputData = contract.methods.multicall(methods).encodeABI();
+
+        // get current account from privkey
+        const currentAccount =
+            web3.eth.accounts.privateKeyToAccount(myPrivateKey);
+        console.log('currentAccount', currentAccount);
+
+        const signResult = await web3.eth.accounts.signTransaction(
+            {
+                to: univ3pos,
+                gasPrice: '42000000000',
+                gas: '80000',
+                data: inputData,
+            },
+            myPrivateKey,
+        );
+
+        const sendResult = await web3.eth.sendSignedTransaction(
+            signResult.rawTransaction,
+        );
+        res.send(sendResult);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
