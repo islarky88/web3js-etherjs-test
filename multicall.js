4fbfc156945cead07077cf75ebafdd64797e2fde
Islarky Overlord <islarky88@gmail.com>
2021-07-08 13:35:39 +0800

    removePool prepare multicall

--- multicall.js
+++ multicall.js
@@ -20,7 +20,9 @@ const univ3pos = '0xc36442b4a4522e871399cd717abdd847ab11fe88';
 
 const contract = new web3.eth.Contract(UNI_V3_POS, univ3pos);
 
-const methods = [];
+let methods = [];
+let func;
+let params;
 
 app.get('/', (req, res) => {
     res.send(`Multicall Uniswap V3
@@ -30,8 +32,9 @@ app.get('/', (req, res) => {
 });
 app.get('/create-pool', async (req, res) => {
     try {
+        methods = [];
         // add mint method first
-        const params = [
+        params = [
             '0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb', // token0 - InstaToken Address
             '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // token1 - WETH9 address
             '123', // fee
@@ -44,12 +47,12 @@ app.get('/create-pool', async (req, res) => {
             '0x7284a8451d9a0e7Dc62B3a71C0593eA2eC5c5638', // recipient - smart contract address of InstaToken
             '1628718978', // deadline
         ];
-        const mintEncoded = contract.methods.mint(params).encodeABI();
-        methods.push(mintEncoded);
+        func = contract.methods.mint(params).encodeABI();
+        methods.push(func);
 
         // add then refundETH next
-        const refund = contract.methods.refundETH().encodeABI();
-        methods.push(refund);
+        func = contract.methods.refundETH().encodeABI();
+        methods.push(func);
 
         const inputData = contract.methods.multicall(methods).encodeABI();
 
@@ -79,26 +82,45 @@ app.get('/create-pool', async (req, res) => {
 });
 app.get('/remove-pool', async (req, res) => {
     try {
-        // add mint method first
-        const params = [
-            '0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb', // token0 - InstaToken Address
-            '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // token1 - WETH9 address
-            '123', // fee
-            '456', // tickLower
-            '789', // tickUpper
-            '249999999999999999716', // amount0Desired
-            '856568008741777183', // amount1Desired
-            '242154525503193061571', // amount0Min
-            '831197723551503890', // amount1Min
-            '0x7284a8451d9a0e7Dc62B3a71C0593eA2eC5c5638', // recipient - smart contract address of InstaToken
-            '1628718978', // deadline
-        ];
-        const mintEncoded = contract.methods.mint(params).encodeABI();
-        methods.push(mintEncoded);
+        func = contract.methods
+            .decreaseLiquidity([
+                '74933', // tokenId
+                '178678694622401229790', // liquidity
+                '173668746393071692156', // amount0Min
+                '601275849210309457', // amount1Min
+                '1625655794', // deadline
+            ])
+            .encodeABI();
+        methods.push(func);
+
+        func = contract.methods
+            .collect([
+                '74933', // tokenId
+                '0x0000000000000000000000000000000000000000', // recipient
+                '340282366920938463463374607431768211455', // amount0Max
+                '340282366920938463463374607431768211455', // amount1Max
+            ])
+            .encodeABI();
+        methods.push(func);
 
         // add then refundETH next
-        const refund = contract.methods.refundETH().encodeABI();
-        methods.push(refund);
+        func = contract.methods
+            .unwrapWETH9([
+                '601302710945574558', // amountMinimum
+                '7284a8451d9a0e7dc62b3a71c0593ea2ec5c5638', // recipient
+            ])
+            .encodeABI();
+        methods.push(func);
+
+        // add then refundETH next
+        func = contract.methods
+            .sweepToken([
+                '6f40d4a6237c257fff2db00fa0510deeecd303eb', // token
+                '173675812114047852235', // amountMinimum
+                '7284a8451d9a0e7dc62b3a71c0593ea2ec5c5638', // recipient
+            ])
+            .encodeABI();
+        methods.push(func);
 
         const inputData = contract.methods.multicall(methods).encodeABI();
 
